95200e29b5b67bd0d4f2cee0c68db6af3dc26f0d
aroxa <cristiano.aroxa@hotmail.com>
2022-09-20 01:36:34 -0300

    banner

---
+++ javascript/script.js
@@ -0,0 +1,29 @@
+let time = 2000,
+    currentImageIndex = 0,
+    images = document
+               .querySelectorAll("#slider img")
+     max = images.length;
+     
+function nextImage() {
+
+    images[currentImageIndex]
+        .classList.remove("selected")
+
+    currentImageIndex++
+
+    if(currentImageIndex >= max)
+         currentImageIndex = 0
+
+    images[currentImageIndex]
+        .classList.add("selected")
+}
+
+function start() {
+    setInterval(() => {
+        console.log("funcao rodada")
+        nextImage()
+    }, time)
+}
+
+
+window.addEventListener("load", start)
